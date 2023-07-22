a4068ba80a50451bffb80ebcc45d2321c4c8c054
Alfred Gaillard <alfred.gaillard@smart-chain.fr>
2023-07-22 17:49:58 +0200

    Putting the right rpc addresses

--- script/ODXScript.sol
+++ script/ODXScript.sol
@@ -1,13 +1,14 @@
    // SPDX-License-Identifier: UNLICENSED
    pragma solidity ^0.8.18;
    import "forge-std/Script.sol";
+   import "forge-std/Test.sol";
    import "../src/ODX.sol";
    contract ODXScript is Script {
        function run() external {
-           uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
-           vm.startBroadcast(deployerPrivateKey);
-           ODX odxContract = new ODX();
-           vm.lo(odxContract);
-           vm.stopBroadcast();
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+        ODX odxContract = new ODX();
+        console.log("ODX contract address: ", address(odxContract));
+        vm.stopBroadcast();
        }
    }
